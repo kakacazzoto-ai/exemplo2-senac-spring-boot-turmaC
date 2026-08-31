cd8ae954244eba68dd26814f5235195176f02345
kakacazzoto-ai <kakacazzoto@gmail.com>
2026-08-31 09:06:54 -0300

    Add files via upload

---
+++ src/main/java/br/senac/tasds/dsw/exemplo2/Exemplo2SpringBootTurmaCApplication.java
@@ -0,0 +1,13 @@
+package br.senac.tasds.dsw.exemplo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exemplo2SpringBootTurmaCApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Exemplo2SpringBootTurmaCApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/senac/tasds/dsw/exemplo2/Exemplo2SpringBootTurmaCApplicationTests.java
@@ -0,0 +1,13 @@
+package br.senac.tasds.dsw.exemplo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exemplo2SpringBootTurmaCApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
